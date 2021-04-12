9acfb29e53f4970d7c4e3d3bbb7707c62a1259a7
Vinson <rosy0616@qq.com>
2021-04-12 15:40:21 +0800

    添加100px = 1 rem适配器

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/style/index.scss'
+import 'normalize.css'
 
 createApp(App).use(store).use(router).mount('#app')
