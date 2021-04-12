836e40dff4c165b282defceabcd2705a295ef292
Vinson <rosy0616@qq.com>
2021-04-12 21:55:58 +0800

    添加Janus库，并能初始化成功

--- src/hooks/useJoinRoom.js
+++ src/hooks/useJoinRoom.js
@@ -1,10 +1,18 @@
 import { reactive } from 'vue'
+import { Janus } from 'janus-gateway'
 
 // 加入房间
 export const useJoinRoom = (room, password) => {
   const roomData = reactive({ room: room, password: password })
   const handleJoin = () => {
     console.log(roomData.room, roomData.password)
+    Janus.init({
+      debug: true,
+      dependencies: Janus.useDefaultDependencies(),
+      callback: () => {
+        console.log('初始化成功')
+      }
+    })
   }
   return {
     roomData,
